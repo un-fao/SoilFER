6ce480874df50361af44b577ec25e7bec702a025
yohannist <yohannist@gmail.com>
2026-06-26 12:18:56 +0300

    fix: update MiniMap component to use TileLayer for MapTiler and add additional WMSTileLayer

--- crop-suitability-app/src/components/location/MiniMap.tsx
+++ crop-suitability-app/src/components/location/MiniMap.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { MapContainer, Marker, useMap, WMSTileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, useMap, WMSTileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -38,19 +38,29 @@ export const MiniMap: React.FC<Props> = ({ position, MapClickHandler }) => (
   <div style={{ borderRadius: '8px', overflow: 'hidden', border: '1px solid #7e5134', backgroundColor: '#fff' }}>
     <MapContainer
       center={position}
-      zoom={MINIMAP_ZOOM}      
+      zoom={MINIMAP_ZOOM}
       maxZoom={MINIMAP_ZOOM}
       zoomControl={false}
       style={{ height: 'clamp(180px, 25vh, 250px)', width: '100%' }}
       attributionControl={false}
     >
-        <WMSTileLayer
-             url="https://data.apps.fao.org/map/gsrv/gsrv1/boundaries/wms?service=WMS"
-             layers="bndl"
-             format="image/png"
-             transparent={true}
-             attribution=''
-           />
+      <TileLayer url={`https://api.maptiler.com/maps/019f0317-b046-74ed-9e5c-200a9e03bd7a/{z}/{x}/{y}.png?key=${process.env.REACT_APP_MAP_KEY}`} attribution="" />
+
+      <WMSTileLayer
+        url="https://data.apps.fao.org/map/gsrv/gsrv1/boundaries/wms?service=WMS"
+        layers="bndl"
+        format="image/png"
+        transparent={true}
+        attribution=''
+      />
+
+      <WMSTileLayer
+        url="https://data.apps.fao.org/map/gsrv/gsrv1/boundaries/wms/v2"
+        layers="cen_uncountries"
+        format="image/png"
+        transparent={true}
+        attribution=''
+      />
       <MapResizer />
       <LocationMarker position={position} />
       {MapClickHandler && <MapClickHandler />}
